5e22872b2ccb779b012d0c520a650242c3d87e2e
bestgoogoo <junta9996@gmail.com>
2022-08-20 14:53:27 +0900

    #6.6 Finicsh
    Last time!!
    Take fake DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,18 +24,43 @@ let videos = [
     id: 3,
   },
 ];
+
 export const trending = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
+
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => {
+
+export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const search = (req, res) => res.send("Search");
-export const remove = (req, res) => res.send("Remove Video");
-export const upload = (req, res) => res.send("Upload Video");
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "Just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
