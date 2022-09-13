35e745b1f6e3b6cdfbd42e3d03b41f75878bc15c
bestgoogoo <junta9996@gmail.com>
2022-09-14 01:11:32 +0900

    #6.28 Finish
    #6에서는 MongoDB와 Mongoose를 이용하여 CRUD를 완성함.
    
    2022.09.14

--- src/db.js
+++ src/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 
+// 니콜라스는 이렇게 mongoose.connect()에 variable을 넣어줬음. 나는 상관없는 듯...
+// mongoose.connect("mongodb://127.0.0.1:27017/wetube", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("🚫 DB Error", error);
