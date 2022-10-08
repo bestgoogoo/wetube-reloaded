a2d6a1507998e27cc31b571bc7947fda12cceb9a
bestgoogoo <junta9996@gmail.com>
2022-10-08 16:25:32 +0900

    Finish GitHub Login
    이제야 GitHub 쓰는 법을 조금은 이해한듯 하다....문제가 발생하면 그거 수정해가는 과정을 전부 Commit 해야하는 것이 맞네.

--- src/middleware.js
+++ src/middleware.js
@@ -8,6 +8,5 @@ export const localsMiddleware = (req, res, next) => {
   // res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
   res.locals.siteName = "Wetube";
-  console.log(res.locals);
   next();
 };
