a399b8ca3ee6bdb570573455e4e15ed240bc7831
bestgoogoo <junta9996@gmail.com>
2022-10-20 20:48:11 +0900

    create restartBtn in recorder.js
    지난 번에 해결하지 못한 restartBtn을 원하는 위치에 넣는 것을 해결했다.
    parentNode.insertBefore()를 알게됨.
    그리고 elementf는 그냥 remove()로 없애면 됨.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -4,30 +4,32 @@ const video = document.getElementById("preview");
 let stream;
 let recorder;
 let videoFile;
-let rerecordBtn;
+let restartBtn;
 
 const handleDownload = () => {
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "Myrecording.webm";
-  document.main.appendChild(a);
+  document.body.appendChild(a);
   a.click();
 };
 
-const handleRerecordBtn = () => {
-  document.main.removeChild(rerecordBtn);
+const handleRestartBtn = () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
+  restartBtn.remove();
   startBtn.innerText = "Start Recording";
   startBtn.removeEventListener("click", handleDownload);
   startBtn.addEventListener("click", handleStart);
 };
 
-const createRerecordBtn = () => {
-  const reset = document.createElement("button");
-  reset.innerText = "Rerecord Audio";
-  reset.id = "rerecordBtn";
-  document.body.appendChild(reset);
-  rerecordBtn = document.getElementById("rerecordBtn");
-  rerecordBtn.addEventListener("click", handleRerecordBtn);
+const createRestartBtn = () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
+  restartBtn = document.createElement("button");
+  restartBtn.innerText = "Restart Recording";
+  restartBtn.id = "restartBtn";
+  startBtn.parentNode.insertBefore(restartBtn, startBtn);
+  restartBtn = document.getElementById("restartBtn");
+  restartBtn.addEventListener("click", handleRestartBtn);
 };
 
 const handleStop = () => {
@@ -35,7 +37,7 @@ const handleStop = () => {
   startBtn.removeEventListener("click", handleStop);
   startBtn.addEventListener("click", handleDownload);
   recorder.stop();
-  createRerecordBtn();
+  createRestartBtn();
 };
 
 const handleStart = () => {
